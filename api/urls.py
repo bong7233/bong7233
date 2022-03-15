1a978099d3ea9b570de0226488cb9da40c5fc9ae
bong7233 <batmantwo7233@gmail.com>
2022-03-15 18:47:28 +0900

    fix

--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import generate_badge, generate_badge_v2, generate_badge_mini, generate_badge_pastel
-
-urlpatterns = [
-    path('generate_badge', generate_badge),
-    path('v2/generate_badge', generate_badge_v2),
-    path('mini/generate_badge', generate_badge_mini),
-    path('pastel/generate_badge', generate_badge_pastel)
-]
